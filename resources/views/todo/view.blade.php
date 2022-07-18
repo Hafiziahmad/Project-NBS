25ef74837d70bac02330cd131dda384baac40c24
Hafiziahmad <hafizifizi3677@gmail.com>
2022-07-18 17:18:06 +0800

    Laravel-List

---
+++ resources/views/todo/view.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h3>List nama intern:</h3>
+    <ol>
+        <li>Yana</li>
+        <li>Fiqa</li>
+        <li>Haziq</li>
+        <li>Hafizi</li>
+        <li>Hazim</li>
+        <li>Luqhman</li>
+    </ol>
+</body>
+</html>
